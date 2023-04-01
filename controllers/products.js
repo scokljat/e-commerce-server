f3916777c6c663022cadb8daeb539c1cb35f2762
scokljat <selma.cokljat@hotmail.com>
2023-04-01 22:48:44 +0200

    Added methods for filtering and pagination

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,5 @@
 const { PrismaClient } = require("@prisma/client");
 
-const res = require("express/lib/response");
-
 const prisma = new PrismaClient();
 
 const getProducts = async (req, res) => {
@@ -9,4 +7,24 @@ const getProducts = async (req, res) => {
   res.status(200).send(products);
 };
 
-module.exports = { getProducts };
+const getPaginatedProducts = async (req, res) => {
+  const page = parseInt(req.query.page);
+  const limit = parseInt(req.query.limit);
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const products = await prisma.products.findMany();
+  const paginatedProducts = products.slice(startIndex, endIndex);
+  res.status(200).send(paginatedProducts);
+};
+
+const getFilteredProducts = async (req, res) => {
+  const category = req.query.category;
+  const products = await prisma.products.findMany();
+  const filteredProducts = products.filter(
+    (product) => product.category === category
+  );
+
+  res.status(200).send(filteredProducts);
+};
+
+module.exports = { getProducts, getPaginatedProducts, getFilteredProducts };
--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,15 @@
 const express = require("express");
 
-const { getProducts } = require("../controllers/products");
+const {
+  getProducts,
+  getPaginatedProducts,
+  getFilteredProducts,
+} = require("../controllers/products");
 
 const router = express.Router();
 
 router.get("/products", getProducts);
+router.get("/products/pagination", getPaginatedProducts);
+router.get("/products/filter", getFilteredProducts);
 
 module.exports = router;
