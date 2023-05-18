d284aabe930d2387fa365757f785c20986a8cbfe
scokljat <selma.cokljat@hotmail.com>
2023-05-18 19:31:36 +0200

    Updated data table

--- validator/validator.js
+++ validator/validator.js
@@ -6,7 +6,6 @@ const validator = (schema) => (payload) =>
 const registerSchema = Joi.object({
   firstName: Joi.string().min(3).max(20).required(),
   lastName: Joi.string().min(3).max(20).required(),
-  userName: Joi.string().min(5).max(20).required(),
   email: Joi.string().email().required(),
   password: Joi.string().min(8).max(20).required(),
 });
